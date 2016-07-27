fc1e91e80c2bf436725f5c2570bbdc52ae4aaf39
pjakubowski <p@weljakubowski.com>
2016-07-27 18:52:06 +0100

    adds css suport to a webpack build

--- js/app.es6
+++
@@ -1,6 +0,0 @@
-// require('./login');
-import {login } from './login';
-login('admin', 'radical');
-document.write('welcome to webpack hey ho hot reloading!!');
-
-console.log('App loaded'); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,24 @@
 var path = require('path');
-var webpack = require('webpack');
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('shared.js');
 
 module.exports = {
     context: path.resolve('js'),
-    entry: {
-        about: './about_page.js',
-        home: './home_page.js',
-        contact: './contact_page.js'
-    },
+    entry: ["./app"],
     output: {
-        path: path.resolve('build/js'),
+        path: path.resolve('build/js/'),
         publicPath: '/public/assets/js/',
-        filename: "[name].js"
+        filename: "bundle.js"
     },
-
-    plugins: [commonsPlugin],
 
     devServer: {
         contentBase: 'public'
     },
 
     module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: 'node_modules',
-                loader: 'jshint-loader'
-        }],
         loaders: [
             {
-                test: /\.es6$/,
+                test: /\.css$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                loader: "style-loader!css-loader"
             }
         ]
     },
