487c3287ce0bcb603c742acf8de81c190644af5e
pjakubowski <p@weljakubowski.com>
2016-07-27 10:52:28 +0100

    adds folder organisation and modifies webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+var path = require('path');
+
 module.exports = {
+    context: path.resolve('js'),
     entry: ["./utils", "./app.js"],
     output: {
+        path: path.resolve('build/js'),
+        publicPath: '/public/assets/js/',
         filename: "bundle.js"
+    },
+
+    devServer: {
+        contentBase: 'public'
     },
 
     module: {
