28fdd8878979f7bcd4327e6c168edd5e052ebdcf
pjakubowski <p@weljakubowski.com>
2016-07-27 11:24:02 +0100

    creating bundles with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 var path = require('path');
+var webpack = require('webpack');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('shared.js');
 
 module.exports = {
     context: path.resolve('js'),
-    entry: ["./utils", "./app"],
+    entry: {
+        about: './about_page.js',
+        home: './home_page.js',
+        contact: './contact_page.js'
+    },
     output: {
         path: path.resolve('build/js'),
         publicPath: '/public/assets/js/',
-        filename: "bundle.js"
+        filename: "[name].js"
     },
+
+    plugins: [commonsPlugin],
 
     devServer: {
         contentBase: 'public'
